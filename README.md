0aa736228814fbc59cc072922cce47f653ac2558
Ahmad-abughaush <126252623+Ahmad-abughaush@users.noreply.github.com>
2023-03-17 21:09:44 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# cafe-z
+**#cafe z 
+at this task i have used several things such as html css and js for creating this web site 
+and googl chrome google qoutes 
+**
---
+++ app.js
@@ -0,0 +1,19 @@
+
+let name = prompt("please enter your name");
+let gender = prompt("please enter your gender ", "male or female ");
+if (gender ==    "male") {
+    alert('welcome mr ' +' '+ name);
+}
+else {
+    alert('welcome ms'+' '+ name);
+}
+
+let drink = confirm("do you want cold or hot drink ");
+if(drink==true){alert ("ok")}
+else {alert('thanks for visiting')} 
+
+let nameofdrink = prompt(" write the drink`s name ");
+{ alert('your drink is getting prepared '); }
+
+console.log( name + " "+ nameofdrink);
+
